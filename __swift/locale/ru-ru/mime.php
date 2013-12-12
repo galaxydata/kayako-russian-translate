08aaff05d0775e0510155f2572820af0f2bf4722
Sonko Dmitry <st.difor@gmail.com>
2013-12-12 10:38:25 +0600

    Initial commit. Added original language files.

---
+++ __swift/locale/ru-ru/mime.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * ###############################################
+ *
+ * SWIFT Framework
+ * _______________________________________________
+ *
+ * @author		Varun Shoor
+ *
+ * @package		SWIFT
+ * @copyright	Copyright (c) 2001-2012, Kayako
+ * @license		http://www.kayako.com/license
+ * @link		http://www.kayako.com
+ *
+ * ###############################################
+ */
+
+$__LANG = array (
+	'pdf' => 'PDF document',
+	'png' => 'PNG image',
+	'htm' => 'HTML document',
+	'html' => 'HTML document',
+	'rtf' => 'Rich text document',
+	'gz' => 'GZIP file',
+	'tar' => 'TAR file',
+	'zip' => 'ZIP file',
+	'rar' => 'RAR file',
+	'bz2' => 'BZ2 file',
+	'bz' => 'BZ file',
+	'jar' => 'JAR file',
+	'doc' => 'Word document',
+	'docx' => 'Word document',
+	'jpg' => 'JPEG image',
+	'jpeg' => 'JPEG image',
+	'txt' => 'Text file',
+	'wav' => 'WAV file',
+	'mov' => 'QuickTime movie',
+	'ppt' => 'PowerPoint document',
+	'pptx' => 'PowerPoint document',
+	'xls' => 'Excel file',
+	'xlsx' => 'Excel file',
+	'ram' => 'Real Audio audio file',
+	'ico' => 'Icon file',
+	'gif' => 'GIF image',
+	'mpg' => 'MPEG video',
+	'mpeg' => 'MPEG video',
+	'mp3' => 'MPEG Audio',
+	'pdf' => 'PDF document',
+	'php' => 'PHP file',
+	'swf' => 'Flash file',
+);
+
+?>
